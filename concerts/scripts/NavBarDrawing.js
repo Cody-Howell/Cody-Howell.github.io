75d9147a3c20fdde944dccc5e5eae11eb60fa668
Cody-Howell <codyhowell44@gmail.com>
2023-03-30 18:09:34 -0600

    everything at once; fixing navbar drawing

--- concerts/scripts/NavBarDrawing.js
+++ concerts/scripts/NavBarDrawing.js
@@ -2,11 +2,11 @@ document.addEventListener('DOMContentLoaded', function() {
     let navbar = document.querySelector("body header");
     navbar.innerHTML = `
     <nav class="navbar">
-    <a href="../index.html"><img src="/images/Treble clef.svg" alt="Treble Logo" class="logo"></a>
+    <a href="/concerts/index.html"><img src="/images/Treble clef.svg" alt="Treble Logo" class="logo"></a>
     <ul class="nav-options">
-        <li><a href="../index.html">Concerts</a></li>
-        <li><a href="../calendar.html">Calendar</a></li>
-        <li><a href="../about.html">About</a></li>
+        <li><a href="/concerts/index.html">Concerts</a></li>
+        <li><a href="/concerts/calendar.html">Calendar</a></li>
+        <li><a href="/concerts/about.html">About</a></li>
     </ul>
     </nav>`;
 });
---
+++ newConcerts/TESTscripts/MainDrawing.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function(){
+    concerts = JSON.parse(concertDatabase);
+    console.log(concerts);
+});
