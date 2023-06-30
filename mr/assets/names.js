1ffb00acd3b03b636e9d870f530bb2c5761de020
Cody-Howell <codyhowell44@gmail.com>
2023-06-29 23:38:06 -0600

    V 1 3, many large updates

--- mr/assets/names.js
+++ mr/assets/names.js
@@ -1,7 +1,22 @@
 const names = [
-    {name: 'Web 1', info: `Inital web I created. I'm happy about where it began, but the ratio of stuff I like is maybe 5:1, dislike-like.`},
-    {name: 'Web 2', info: `Second web I created, solo. I like a lot more of the nodes, maybe a 4:1 like-dislike. `},
-    {name: 'Web 3', info: `Newest web, or Percussion web. Currently being built.`}
+    {   
+        name: 'Web 1', 
+        info: `Inital web I created. I'm happy about where it began, but the ratio of stuff I like is maybe 5:1, dislike-like.`,
+        author_route: 'Hello', 
+        seed: 2
+    },
+    {   
+        name: 'Web 2', 
+        info: `Second web I created, solo. I like a lot more of the nodes, maybe a 4:1 like-dislike. `,
+        author_route: 'Hello',
+        seed: 1
+    },
+    {   
+        name: 'Web 3', 
+        info: `Newest web, or Percussion web. Currently being built.`,
+        author_route: 'Hello',
+        seed: 2
+    }
 ];
 
 export {names}
--- mr/script.js
+++ mr/script.js
@@ -1,4 +1,5 @@
-var currentTitle, currentNum, nextTitle, nextNum, nextSelectedNum, prevNum, nodes, edges, musicData;
+var currentTitle, currentNum, nextTitle, nextNum, nextSelectedNum, nodes, edges, musicData, playedString;
+var prevNum = -1;
 var webChoice = 0;
 var audio = []; 
 var start = [];
@@ -16,16 +17,10 @@ for (var i = 0; i < names.length; i++) {
 
 selectElement.addEventListener("change", function() {
   webChoice = this.value;
-  console.log("Selected ID:", webChoice);
-  audio.forEach((audioTrack) => {
-    if (!audioTrack.paused) {
-      audioTrack.pause();
-      audioTrack.currentTime = 0;  // Reset the audio to the start
-      clearTimeout(timeouts[i]);
-    }
-  });
-  startup();
+  console.log("SilDEBUG: Selected Web ID is ", webChoice);
+  stopAudio();
 });
+
 (function() {
 	'use strict';
 
@@ -43,7 +38,7 @@ async function startup() {
 
   audio = [];
   start = [];
-
+  playedString = "";
 
 
   var filepath = `./assets/${names[webChoice].name}/music-data.json`;
@@ -58,12 +53,14 @@ async function startup() {
   nextNum = start[Math.floor(Math.random() * start.length)];
   nextTitle = musicData[nextNum].title;
   nextSelectedNum = -1;
-  // document.getElementsByClassName('table-row-container')[nextNum].children[0].children[0].classList.add('next-cell');
+  updateRightColumn(nextTitle, 'N/A', musicData[nextNum].artist, []);
+
   
   document.getElementById('controls-container').innerHTML =
-          `<button id="begin-btn">Begin</button>`;
+          `<button id="begin-btn">Begin</button>\n<p id="details-box">${names[webChoice].info}</p>`;
   document.querySelector('#begin-btn').addEventListener('click', function(){
-    document.getElementById('controls-container').innerHTML =`<p id="details-box">${names[webChoice].info}</p>`;
+    document.getElementById('controls-container').innerHTML =`<button id="stop-btn">Stop</button>\n<p id="details-box">${names[webChoice].info}</p>`;
+    document.querySelector('#stop-btn').addEventListener('click', stopAudio);
     playNext();
   });
 
@@ -81,29 +78,33 @@ function loadTable() {
       let color;
       switch(music.start) {
           case true:
+              start.push(index);
               color = "#47d16c";
               break;
           default:
               color = "#DDDDDD";
               break;
       }
-    
+      
       // Generate node array
       let node = {
           id: index,
+          value: 5,
           label: music.title,
-          color: color
+          color: color, 
+          title: `Artist: ${music.artist} \nDuration: ${secondsToMS(music.lengthToNext)}`
       };
       nodeArray.push(node);
     
       // Generate edge array
       music.next.forEach((nextNode) => {
           // Get the index of the nextNode from the musicData array
-          let toIndex = musicData.findIndex(item => item.title === nextNode);
+          let toIndex = musicData.findIndex(item => item.title === nextNode.name);
           if (toIndex != -1) {
               let edge = {
                   from: index,
                   to: toIndex,
+                  value: nextNode.weight,
                   arrows: {to: {enabled: true, type: "arrow"}},
                   color: "#000000"
               };
@@ -124,8 +125,36 @@ function loadTable() {
   var options = {
     nodes: {
       shape: "circle",
+      scaling: {
+        label: {
+          enabled: true,
+          min: 40,
+          max: 40
+        }
+      }
+    },
+    edges: {
+      scaling: {
+        customScalingFunction: function (min, max, total, value) {
+            return (value - min) / (max - min);
+        },
+        min: 1,
+        max: 5,
+      },
+    },
+    layout: {randomSeed: names[webChoice].seed},
+    physics: {
+      barnesHut: {
+        gravitationalConstant: -8000,
+        centralGravity: 0.3,
+        springLength: 200,
+        springConstant: 0.08,
+        damping: 0.20,
+        avoidOverlap: 1
+      }
     }
-  };
+};
+
   var network = new vis.Network(container, data, options);
   
   console.log('  EisDEBUG: loadTable() completed.');
@@ -135,16 +164,6 @@ function prepareAudio() {
   console.log('  EisDEBUG: prepareAudio() started; musicData.length is ' + musicData.length);
   for(let i = 0; i < musicData.length; i++) {
     audio.push(new Audio(`assets/${names[webChoice].name}/audio-clips/` + musicData[i].file));
-    audio[i].addEventListener('play', function() {
-      if (timeouts[i]) {
-        clearTimeout(timeouts[i]);
-      }
-      // Set a new timeout and store its ID
-      timeouts[i] = setTimeout(playNext, (musicData[i].lengthToNext) - 10); // Arbitrary number to reduce silence
-    });
-    if (musicData[i].start) {
-      start.push(i);
-    }
   }
   console.log('  EisDEBUG: prepareAudio() completed; audio.length is ' + audio.length);
   console.log('  EisDEBUG: prepareAudio() completed; start.length is ' + start.length);
@@ -152,7 +171,6 @@ function prepareAudio() {
 
 function playNext() {
   console.log('  EisDEBUG: playNext(); nextNum is ' + nextNum);
-
   
   
   if(nextNum === -1) {
@@ -160,33 +178,23 @@ function playNext() {
     nextNum = 0;
     nextTitle = musicData[nextNum].title;
     nextSelectedNum = -1;
-    // document.getElementsByClassName('table-row-container')[currentNum].children[0].children[0].classList.remove('current-cell');
-    // document.getElementsByClassName('table-row-container')[nextNum].children[0].children[0].classList.add('next-cell');
   } else {
     audio[nextNum].play();
     console.log('    EisDEBUG: playing track ' + nextNum);
     
-    // decolor
-    // document.getElementsByClassName('table-row-container')[nextNum].children[0].children[0].classList.remove('next-cell');
-    if(nextSelectedNum !== -1) {
-      // document.getElementsByClassName('table-row-container')[currentNum].children[1].children[nextSelectedNum].classList.remove('next-cell');
-    }
-    if(currentNum !== -1) {
-      // document.getElementsByClassName('table-row-container')[currentNum].children[0].children[0].classList.remove('current-cell');
-    }
-    
     currentTitle = nextTitle;
     currentNum = nextNum;
-    // document.getElementsByClassName('table-row-container')[currentNum].children[0].children[0].classList.add('current-cell');
+    timeouts.push(setTimeout(playNext, musicData[currentNum].lengthToNext));
+
+    simulateProgress(musicData[currentNum].lengthToNext);
     
     if(musicData[nextNum].next.length === 0) {
       console.log('  EisDEBUG: no next song.');
-      // document.getElementsByClassName('table-row-container')[currentNum].children[1].children[0].classList.add('end-cell');
       nextNum = -1;
     } else {
       // choose next
-      nextSelectedNum = Math.floor(Math.random() * musicData[currentNum].next.length);
-      nextTitle = musicData[currentNum].next[nextSelectedNum];
+      nextSelectedNum = weightedRandomIndex(musicData[currentNum].next);
+      nextTitle = musicData[currentNum].next[nextSelectedNum].name;
       
       for(let i = 0; i < musicData.length; i++) {
         // console.log(`      EisDEBUG: musicData[${i}].title is ${musicData[i].title}`);
@@ -221,7 +229,111 @@ function playNext() {
       
       nodes.update({id: nextNum, color: '#e0d679'});
       nodes.update({id: currentNum, color: '#5e71c4'}); 
+
+      updateRightColumn(currentTitle, nextTitle, musicData[currentNum].artist, musicData[currentNum].next);
+      updatePlayString(currentTitle);
       
     }
   }
+}
+
+function updateRightColumn(current, next, artist, nextList) {
+  var currentNode = document.querySelector("#current .largeBold");
+  var nextNode = document.querySelector("#nextNode");
+  var artistLine = document.querySelector("#RightArtist");
+  var nodeList = document.querySelector("#nodeList");
+  var longNodeList = document.querySelector("#secondNodeList");
+
+  if (next !== "N/A"){
+    let nextNames = [];
+    let node = musicData.find(item => next === item.title);
+    for (let nextNode of node.next) {
+      nextNames.push(nextNode.name);
+    }
+    longNodeList.innerHTML = ``;
+    nextNames.forEach((item) => {
+      let node = document.createElement('p');
+      node.innerHTML = `${item}`;
+      longNodeList.appendChild(node);
+    });
+  } else {
+    longNodeList.innerHTML = ``;
+  }
+  
+  nextList = nextList.filter(item => item.name !== next);
+
+  currentNode.innerHTML = `${current}`;
+  nextNode.innerHTML = `${next}`;
+  artistLine.innerHTML = `${artist}`;
+  nodeList.innerHTML = ``;
+  nextList.forEach((item) => {
+    let node = document.createElement('p');
+    node.innerHTML = `${item.name} (${item.weight})`;
+    nodeList.appendChild(node);
+  });
+}
+
+function updatePlayString(current) {
+  var playNode = document.querySelector("#results p");
+
+  playedString += current + ", ";
+
+  playNode.innerHTML = `${playedString}`;
+}
+
+function stopAudio() {
+  audio.forEach((audioTrack) => {
+    if (!audioTrack.paused) {
+      audioTrack.pause();
+      audioTrack.currentTime = 0;  // Reset the audio to the start
+      clearTimeout(timeouts[i]);
+    }
+  });
+  timeouts.forEach((e, i) => {
+    clearTimeout(timeouts[i]);
+  });
+  timeouts = [];
+  audio = [];
+  prevNum = -1;
+  startup();
+};
+
+function simulateProgress(totalTime) {
+  var progressBar = document.getElementById('progress-bar');
+  var startTime = Date.now();
+  var endTime = startTime + totalTime;
+
+  var interval = setInterval(updateProgressBar, 100);
+
+  function updateProgressBar() {
+    var currentTime = Date.now();
+    var elapsedTime = currentTime - startTime;
+    var remainingTime = endTime - currentTime;
+    var progress = (elapsedTime / totalTime) * 100;
+    var remainingProgress = 100 - progress;
+
+    if (currentTime >= endTime) {
+      clearInterval(interval);
+      progressBar.style.width = '100%';
+    } else {
+      progressBar.style.width = progress + '%';
+    }
+  }
+};
+
+function weightedRandomIndex(arr) {
+  let totalWeight = arr.reduce((accum, item) => accum + item.weight, 0);
+  let randomNum = Math.random() * totalWeight;
+
+  for(let i = 0; i < arr.length; i++) {
+      if (randomNum <= arr[i].weight) {
+          return i;
+      }
+      randomNum -= arr[i].weight;
+  }
+}
+
+function secondsToMS(num){
+  let returnValue = num / 1000;
+  return `${returnValue} s`
 }
